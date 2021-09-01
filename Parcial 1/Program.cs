37ffca7e51ceb695f898c011bb0e08d4607197c7
HectorFranc0u <halejandrofb@gmail.com>
2021-09-01 17:01:11 -0600

    arreglo del menu

--- Parcial 1/Program.cs
+++ Parcial 1/Program.cs
@@ -18,25 +18,48 @@ namespace Parcial_1
             string path = @"C:\Users\hecto\source\repos\Parcial 1\Parcial 1\archivos\contrasenas.txt";
             return path;
         }
-        private static bool inicio()
-        {
-            Console.WriteLine("------------------------------------------------");
-            Console.WriteLine("1 - INICIAR SESION.");
-            Console.WriteLine("2 - REGISTRAR USUARIO.");
-            Console.WriteLine("------------------------------------------------");
-            Console.WriteLine("SELECCIONE UNA OPCION: ");
 
-            switch (Console.ReadLine())
+        private static void inicio()
+        {
+            string adminuser = "admin";
+            string adminpass = "123";
+            Console.WriteLine("USUARIO.");
+            var user = Console.ReadLine();
+
+            if (search(user))
             {
-                case "1":
-                    comprobar();
-                    Console.ReadKey();
-                    return true;
-                case "2":
-                    registro();
-                    return true;
-                default:
-                    return false;
+                Console.WriteLine("EL USUARIO EXISTE.");
+                Console.WriteLine("");
+                Console.WriteLine("CONTRASENA");
+                var pass = Console.ReadLine();
+
+                if (!read().ContainsValue(pass))
+                {
+                    Console.Write("CONTRASENA CORRECTA");
+                    Console.Write("");
+                    Console.Write("USTED INGRESO COMO INVITADO.");
+                }
+                else
+                {
+                    Console.Write("CONTRASENA INCORRECTA");
+                }
+            }
+            else if (user == adminuser)
+            {
+                Console.WriteLine("EL USUARIO PERTENECE AL ADMINISTRADOR.");
+                Console.WriteLine("");
+                Console.WriteLine("CONTRASENA");
+                var pass = Console.ReadLine();
+                if (pass == adminpass)
+                {
+                    Console.Write("CONTRASENA CORRECTA");
+                    Console.WriteLine("");
+                    Console.Write("USTED INGRESO COMO ADMINISTRADOR.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("EL USUARIO NO EXISTE.");
             }
         }
         private static void registro()
@@ -53,7 +76,6 @@ namespace Parcial_1
                 pr.WriteLine("{0}; {1}", newus, newpass);
                 pr.Close();
             }
-            inicio();
         }
 
         private static Dictionary<object, object> read()
@@ -84,55 +106,7 @@ namespace Parcial_1
             else
             {
                 return true;
-            }   
-        }
-
-        private static bool comprobar()
-        {
-            string adminuser = "admin";
-            string adminpass = "123";
-            Console.WriteLine("USUARIO.");
-            var user = Console.ReadLine();
-
-            if (search(user))
-            {
-                Console.WriteLine("EL USUARIO EXISTE.");
-                Console.WriteLine("");
-                Console.WriteLine("CONTRASENA");
-                var pass = Console.ReadLine();
-
-                if (!read().ContainsValue(pass))
-                {
-                    Console.Write("CONTRASENA CORRECTA");
-                    Console.Write("USTED INGRESO COMO INVITADO.");
-                    return false;
-                }
-                else
-                {
-                    Console.Write("CONTRASENA INCORRECTA");
-                    return true;
-                }
             }
-            else if (user == adminuser)
-            {
-                Console.WriteLine("EL USUARIO PERTENECE AL ADMINISTRADOR.");
-                Console.WriteLine("");
-                Console.WriteLine("CONTRASENA");
-                var pass = Console.ReadLine();
-                if (pass == adminpass)
-                {
-                    Console.Write("CONTRASENA CORRECTA");
-                    Console.WriteLine("");
-                    Console.Write("USTED INGRESO COMO ADMINISTRADOR.");
-                }
-                return false;
-            }
-            else
-            {
-                Console.WriteLine("EL USUARIO NO EXISTE.");
-                return false;
-
-            }    
         }
     }
 }
