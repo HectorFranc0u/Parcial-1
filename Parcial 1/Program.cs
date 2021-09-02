bf446142e461480e36a83c58b7c19beb9a043f3b
geomalv02 <smis063721@ugb.edu.sv>
2021-09-01 18:57:31 -0600

    se crea la funcion para cerrar la consola

--- Parcial 1/Program.cs
+++ Parcial 1/Program.cs
@@ -18,25 +18,49 @@ namespace Parcial_1
             string path = @"C:\Users\hecto\source\repos\Parcial 1\Parcial 1\archivos\contrasenas.txt";
             return path;
         }
-        private static bool inicio()
-        {
-            Console.WriteLine("------------------------------------------------");
-            Console.WriteLine("1 - INICIAR SESION.");
-            Console.WriteLine("2 - REGISTRAR USUARIO.");
-            Console.WriteLine("------------------------------------------------");
-            Console.WriteLine("SELECCIONE UNA OPCION: ");
 
-            switch (Console.ReadLine())
+        private static void inicio()
+        {
+            string adminuser = "admin";
+            string adminpass = "123";
+            Console.WriteLine("USUARIO.");
+            var user = Console.ReadLine();
+
+            if (search(user))
             {
-                case "1":
-                    comprobar();
-                    Console.ReadKey();
-                    return true;
-                case "2":
-                    registro();
-                    return true;
-                default:
-                    return false;
+                Console.WriteLine("EL USUARIO EXISTE.");
+                Console.WriteLine("");
+                Console.WriteLine("CONTRASENA");
+                var pass = Console.ReadLine();
+
+                if (!read().ContainsValue(pass))
+                {
+                    Console.Write("CONTRASENA CORRECTA");
+                    Console.Write("");
+                    Console.Write("USTED INGRESO COMO INVITADO.");
+                }
+                else
+                {
+                    Console.Write("CONTRASENA INCORRECTA");
+                }
+            }
+            else if (user == adminuser)
+            {
+                Console.WriteLine("EL USUARIO PERTENECE AL ADMINISTRADOR.");
+                Console.WriteLine("");
+                Console.WriteLine("CONTRASENA");
+                var pass = Console.ReadLine();
+                if (pass == adminpass)
+                {
+                    Console.Write("CONTRASENA CORRECTA");
+                    Console.WriteLine("");
+                    Console.Write("USTED INGRESO COMO ADMINISTRADOR.");
+                    menuadmin();
+                }
+            }
+            else
+            {
+                Console.WriteLine("EL USUARIO NO EXISTE.");
             }
         }
         private static void registro()
@@ -53,7 +77,7 @@ namespace Parcial_1
                 pr.WriteLine("{0}; {1}", newus, newpass);
                 pr.Close();
             }
-            inicio();
+            menuadmin();
         }
 
         private static Dictionary<object, object> read()
@@ -84,54 +108,56 @@ namespace Parcial_1
             else
             {
                 return true;
-            }   
+            }
         }
 
-        private static bool comprobar()
+        // menu de administrador
+        private static bool menuadmin()
         {
-            string adminuser = "admin";
-            string adminpass = "123";
-            Console.WriteLine("USUARIO.");
-            var user = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("BIENVENIDO AL MENU DE ADMINISTRADOR.");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("1 - CREAR UN USUARIO DE INVITADO.");
+            Console.WriteLine("2 - CREAR EL ARCHIVO CON SU EXPERIENCIA PERSONAL.");
+            Console.WriteLine("3 - BORRAR EL ARCHIVO CREADO.");
+            Console.WriteLine("4 - CERRAR LA CONSOLA.");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("SELECCIONE UNA OPCION. ");
 
-            if (search(user))
+            switch (Console.ReadLine())
             {
-                Console.WriteLine("EL USUARIO EXISTE.");
-                Console.WriteLine("");
-                Console.WriteLine("CONTRASENA");
-                var pass = Console.ReadLine();
-
-                if (!read().ContainsValue(pass))
-                {
-                    Console.Write("CONTRASENA CORRECTA");
-                    Console.Write("USTED INGRESO COMO INVITADO.");
-                    return false;
-                }
-                else
-                {
-                    Console.Write("CONTRASENA INCORRECTA");
+                case "1":
+                    registro();
                     return true;
-                }
+                case "2":
+                    docalumno();
+                    return true;
+                case "3":
+                    return true;
+                case "4":
+                    Console.WriteLine("PRESIONE CUALQUIER TECLA PARA CERRAR LA CONSOLA.");
+                    return true;
+                default:
+                    return false;
             }
-            else if (user == adminuser)
-            {
-                Console.WriteLine("EL USUARIO PERTENECE AL ADMINISTRADOR.");
-                Console.WriteLine("");
-                Console.WriteLine("CONTRASENA");
-                var pass = Console.ReadLine();
-                if (pass == adminpass)
-                {
-                    Console.Write("CONTRASENA CORRECTA");
-                    Console.WriteLine("");
-                    Console.Write("USTED INGRESO COMO ADMINISTRADOR.");
-                }
-                return false;
-            }
-            else
-            {
-                Console.WriteLine("EL USUARIO NO EXISTE.");
-                return false;
-            }    
+        }
+        private static void docalumno()
+        {
+            Console.Clear();
+            Console.WriteLine("ARCHIVO CON SU EXPERIENCIA PERSONAL.");
+            Console.WriteLine("INGRESE SU NOMBRE.");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("ESCRIBA SU EXPERIENCIA.");
+            string experiencia = Console.ReadLine();
+
+            StreamWriter sw = File.CreateText(nombre);
+            sw.WriteLine(experiencia);
+            sw.Dispose();
+            menuadmin();
+        }
+        private static void borrardoc()
+        {
+
         }
     }
 }
